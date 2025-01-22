56574ef8a94e7d05901b4f7873b7f3f93e1bd34a
crypto <jaysonmatthew2005@gmail.com>
2025-01-22 21:26:27 +0100

    fixed missing import in interpreter

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,9 +1,12 @@
+# Importiere die AST-Klassen
+from parser import BinOpNode, NumNode, AssignNode, VarNode, PrintNode
+
 class Interpreter:
     def __init__(self):
-        self.variables = {}
+        self.variables = {}  # Variablenspeicher
 
     def visit(self, node):
-        if isinstance(node, BinOpNode):
+        if isinstance(node, BinOpNode):  # Mathematische Operationen
             left = self.visit(node.left)
             right = self.visit(node.right)
             if node.op == "+":
@@ -14,13 +17,16 @@ class Interpreter:
                 return left * right
             elif node.op == "/":
                 return left / right
-        elif isinstance(node, NumNode):
+        elif isinstance(node, NumNode):  # Zahlen
             return node.value
-        elif isinstance(node, VarNode):
-            return self.variables.get(node.name, 0)
-        elif isinstance(node, AssignNode):
+        elif isinstance(node, VarNode):  # Variablen
+            if node.name not in self.variables:
+                raise NameError(f"Variable '{node.name}' ist nicht definiert.")
+            return self.variables[node.name]
+        elif isinstance(node, AssignNode):  # Zuweisungen
             self.variables[node.name] = self.visit(node.value)
-        elif isinstance(node, PrintNode):
+        elif isinstance(node, PrintNode):  # Druckbefehl
             value = self.visit(node.expr)
             print(value)
-
+        else:
+            raise TypeError(f"Unbekannter Knotentyp: {type(node)}")
